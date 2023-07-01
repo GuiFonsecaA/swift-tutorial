1ea357dca94018890241addbfbea5567a996b69b
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-01 16:31:53 -0300

    Initial Commit

---
+++ GuilhermeTutorial/GuilhermeTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuilhermeTutorialApp.swift
+//  GuilhermeTutorial
+//
+//  Created by Guilherme Fonseca on 01/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GuilhermeTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
