cf27f43653a7f29fae66b5848dcd85ad578c69bb
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-14 19:46:49 -0300

    Projeto inicial

---
+++ GuilhermeTutorial/NavigationBar/NavigationBar.swift
@@ -0,0 +1,27 @@
+//
+//  NavigationBar.swift
+//  GuilhermeTutorial
+//
+//  Created by Guilherme Fonseca on 14/07/23.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        HStack{
+            Button("Rua Qualquer, 157"){
+                
+            }
+            Button("Notificações"){
+                
+            }
+        }
+    }
+}
+
+struct NavigationBar_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBar()
+    }
+}
