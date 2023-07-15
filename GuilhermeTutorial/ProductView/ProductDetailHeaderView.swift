cf9ea5875603fedc9bd5977140f4bf83dfa32a65
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-15 20:41:12 -0300

    Componentização da view de detalhes do produto

---
+++ GuilhermeTutorial/ProductView/ProductDetailHeaderView.swift
@@ -0,0 +1,40 @@
+//
+//  ProductDetailHeaderView.swift
+//  GuilhermeTutorial
+//
+//  Created by Guilherme Fonseca on 15/07/23.
+//
+
+import SwiftUI
+
+struct ProductDetailHeaderView: View {
+    let product: ProductType
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Image(product.image)
+                .resizable()
+                .scaledToFit()
+                .shadow(radius: 20)
+            
+            Text(product.name)
+                .font(.title)
+                .bold()
+                .padding(.horizontal)
+                .padding(.top)
+            
+            Text(product.description)
+                .padding(.horizontal)
+            
+            Text(product.formattedPrice)
+                .font(.title3)
+                .bold()
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct ProductDetailHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailHeaderView(product: storesMock[0].products[0])
+    }
+}
