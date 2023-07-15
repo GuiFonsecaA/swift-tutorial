7a511b98489722622ef734d924660429b8cb96aa
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-15 15:39:46 -0300

    Alteração da navegação default do botão voltar

--- GuilhermeTutorial/StoresView/StoreDetailView.swift
+++ GuilhermeTutorial/StoresView/StoreDetailView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct StoreDetailView: View {
+    
     let store: StoreType
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack(alignment: .leading) {
@@ -71,6 +74,22 @@ struct StoreDetailView: View {
             }
             .navigationBarTitle(store.name)
         .navigationBarTitleDisplayMode(.inline)
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    HStack (spacing: 4){
+                        Image(systemName: "cart")
+                        
+                        Text("Lojas")
+                    }
+                    .foregroundColor(Color("ColorRed"))
+                }
+
+            }
+        }
         }
     }
 }
