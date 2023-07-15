b8d1f0fae7744bccbebb4b3259760bc4c31e8ecc
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-14 21:37:28 -0300

    Inclusão do GridView de Tipos

---
+++ GuilhermeTutorial/Model/OrderType.swift
@@ -0,0 +1,14 @@
+//
+//  OrderType.swift
+//  GuilhermeTutorial
+//
+//  Created by Guilherme Fonseca on 14/07/23.
+//
+
+import Foundation
+
+struct OrderType: Identifiable {
+    let id: Int
+    let name: String
+    let image: String
+}
