80452323ceab07005eb8ef93e14980a0ac9ac5f0
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-15 22:38:03 -0300

    Inclusão do botão que receberá um efeito DragGesture

--- GuilhermeTutorial/HomeView/HomeView.swift
+++ GuilhermeTutorial/HomeView/HomeView.swift
@@ -11,6 +11,7 @@ struct HomeView: View {
     
     @State private var isAnimating = false
     @State private var imageOffSet: CGSize = .zero
+    let buttonHeight: CGFloat = 80
     
     var body: some View {
         GeometryReader { geometry in
@@ -66,6 +67,40 @@ struct HomeView: View {
                                     }
                                 })
                         )
+                    
+                    ZStack {
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                        
+                        Capsule()
+                            .fill(Color("ColorRed").opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Descobra mais")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(Color("ColorRedDark"))
+                            .offset(x: 20)
+                        
+                        HStack{
+                            ZStack{
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                
+                                Circle()
+                                    .fill(Color("ColorRedDark"))
+                                    .padding(8)
+                                
+                                Image(systemName: "chevron.right.2")
+                                    .font(.system(size: 24))
+                                    .bold()
+                                    .foregroundColor(.white)
+                            }
+                            Spacer()
+                        }
+                    }
+                    .frame(width: geometry.size.width - 60,
+                    height: buttonHeight)
                 }
                 .onAppear{
                     withAnimation(.easeInOut(duration: 4.5)){
