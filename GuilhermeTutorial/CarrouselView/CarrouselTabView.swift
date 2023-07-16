2eaaa455e52e9c53648fdd67c478bd23a94e8646
GuilhermeAFonseca <guilherme.araujo.f@gmail.com>
2023-07-15 23:15:17 -0300

    Inclusão da troca automatica dos banners do carrosel com Timer schedule

--- GuilhermeTutorial/CarrouselView/CarrouselTabView.swift
+++ GuilhermeTutorial/CarrouselView/CarrouselTabView.swift
@@ -8,19 +8,31 @@
 import SwiftUI
 
 struct CarrouselTabView: View {
+    
+    @State private var currentIndex = 1
+    
     let ordersMock: [OrderType] = [
         OrderType(id: 1, name: "", image: "barbecue-banner"),
         OrderType(id: 2, name: "", image: "brazilian-meal-banner"),
         OrderType(id: 3, name: "", image: "pokes-banner"),
     ]
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(ordersMock){ mock in
                 CarrouselItemView(order: mock)
+                    .tag(mock.id)
             }
         }
         .frame(height: 180)
         .tabViewStyle(.page(indexDisplayMode: .always))
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                if currentIndex > ordersMock.count {
+                    currentIndex = 1
+                }
+                currentIndex += 1
+            }
+        }
     }
 }
 
